b4adfd5daa7b26ce18fa40204a7337addd4d1556
thrinadhram <110017774+thrinadhram@users.noreply.github.com>
2023-12-15 11:24:55 +0530

    Add files via upload
    commit in frst

---
+++ src/amma143/Nagamani143.java
@@ -0,0 +1,22 @@
+package amma143;
+
+public class Nagamani143 {
+static {
+	System.out.println("happy birthday to thrinadh sir");
+}
+{
+	System.out.println("i love my family");
+}
+
+
+
+public Nagamani143() {
+	System.out.println("i miss u amma and  love u amma");
+}
+	public static void main(String[] args) {
+		Nagamani143 nm = new Nagamani143();
+		
+
+	}
+
+}
